b43901f762d28ba17ac9ceda0e40a07bc86ae709
Sheshank <sheshankmysa@gmail.com>
2025-03-11 21:58:15 +0530

    Add files via upload

---
+++ OOPS LAB/BASIC JAVA PROGRAMS/PalindromeCheck.java
@@ -0,0 +1,18 @@
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        int num = 121; int original = num; int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
+}
